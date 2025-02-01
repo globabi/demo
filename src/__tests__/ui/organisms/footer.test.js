45d87e15fe75ac9925f6e4607aebfc40901e7d40
alien <jamesiwelsh@hotmail.com>
2025-02-01 09:37:01 +0000

    updated footer and header tests after multilingual support addition

--- src/__tests__/ui/organisms/footer.test.js
+++ src/__tests__/ui/organisms/footer.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import Footer from '../../../ui/organisms/footer'
+import { EN } from '../../../content/language/en'
 
 describe('testing footer', () => {
   test('rendering footer', () => {
-    render(<Footer />)
+    render(<Footer language={EN} />)
   })
 })
--- src/__tests__/ui/organisms/header.test.js
+++ src/__tests__/ui/organisms/header.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import Header from '../../../ui/organisms/header'
+import { EN } from '../../../content/language/en'
 
 describe('testing header', () => {
   test('rendering header', () => {
-    render(<Header />)
+    render(<Header language={EN} currentLanguage={"EN"} />)
   })
 })
